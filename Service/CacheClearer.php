7445500961e1cbfd14ab759355cbcd66dbca6041
Gordon Franke <info@nevalon.de>
2026-09-23 21:52:03 +0200

    refactor: simplify CacheClearer service for Symfony 6+ - Remove LegacyTranslatorInterface compatibility check - Add direct type hint for TranslatorInterface - Remove deprecated code comments about sf 4.2 - Simplify constructor validation

--- Service/CacheClearer.php
+++ Service/CacheClearer.php
@@ -14,7 +14,6 @@ namespace Translation\Bundle\Service;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\CacheWarmer\WarmableInterface;
-use Symfony\Component\Translation\TranslatorInterface as LegacyTranslatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
@@ -39,14 +38,8 @@ final class CacheClearer
      */
     private $filesystem;
 
-    public function __construct(string $kernelCacheDir, $translator, Filesystem $filesystem)
+    public function __construct(string $kernelCacheDir, TranslatorInterface $translator, Filesystem $filesystem)
     {
-        // The TranslatorInterface has been deprecated in favor of Symfony\Contracts\Translation\TranslatorInterface in sf4.2.
-        // Use this class to type hint event & remove the following condition once sf ^4.2 become the minimum supported version.
-        // @see https://github.com/symfony/symfony/blob/master/UPGRADE-4.2.md#translation
-        if (!$translator instanceof LegacyTranslatorInterface && !$translator instanceof TranslatorInterface) {
-            throw new \InvalidArgumentException('Unable to deal with the given translator.');
-        }
 
         $this->kernelCacheDir = $kernelCacheDir;
         $this->translator = $translator;
