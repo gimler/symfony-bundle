2a98e275d3777b9b6974bc3314f39d56b06de695
Gordon Franke <info@nevalon.de>
2026-09-23 21:57:51 +0200

    test: remove Kernel::VERSION check from EditInPlaceTest - Remove Kernel::VERSION_ID check for Symfony 5.3 compatibility - Remove reference to removed disabled_label_legacy.yaml - Remove unused Kernel import - Use only disabled_label.yaml configuration

--- Tests/Functional/Controller/EditInPlaceTest.php
+++ Tests/Functional/Controller/EditInPlaceTest.php
@@ -14,7 +14,6 @@ namespace Translation\Bundle\Tests\Functional\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
-use Symfony\Component\HttpKernel\Kernel;
 use Translation\Bundle\EditInPlace\Activator;
 use Translation\Bundle\Tests\Functional\BaseTestCase;
 
@@ -59,11 +58,7 @@ class EditInPlaceTest extends BaseTestCase
 
     public function testIfUntranslatableLabelGetsDisabled(): void
     {
-        if (Kernel::VERSION_ID < 50300) {
-            $this->testKernel->addTestConfig(__DIR__.'/../app/config/disabled_label_legacy.yaml');
-        } else {
-            $this->testKernel->addTestConfig(__DIR__.'/../app/config/disabled_label.yaml');
-        }
+        $this->testKernel->addTestConfig(__DIR__.'/../app/config/disabled_label.yaml');
         $this->testKernel->boot();
         $request = Request::create('/foobar');
 
