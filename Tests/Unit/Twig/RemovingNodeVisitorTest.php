959108bf78ae096b763df809a28f2d25050a013f
Gordon Franke <info@nevalon.de>
2026-09-23 21:56:02 +0200

    fix: remove incomplete Kernel::VERSION references in tests - Fix RemovingNodeVisitorTest to remove version_compare condition - Kernel::VERSION was not imported and would cause runtime error - Remove outdated comments about Symfony 5.0 compatibility

--- Tests/Unit/Twig/RemovingNodeVisitorTest.php
+++ Tests/Unit/Twig/RemovingNodeVisitorTest.php
@@ -11,7 +11,6 @@
 
 namespace Translation\Bundle\Tests\Unit\Twig;
 
-
 /**
  * @author Johannes M. Schmitt <schmittjoh@gmail.com>
  */
@@ -19,15 +18,8 @@ class RemovingNodeVisitorTest extends BaseTwigTestCase
 {
     public function testRemovalWithSimpleTemplate(): void
     {
-        // transchoice tag have been definively removed in sf ^5.0
-        // Remove this condition & *with_transchoice templates once sf ^5.0 is the minimum supported version.
-        if (version_compare(Kernel::VERSION, 5.0, '<')) {
-            $expected = $this->parse('simple_template_compiled_with_transchoice.html.twig');
-            $actual = $this->parse('simple_template_with_transchoice.html.twig');
-        } else {
-            $expected = $this->parse('simple_template_compiled.html.twig');
-            $actual = $this->parse('simple_template.html.twig');
-        }
+        $expected = $this->parse('simple_template_compiled.html.twig');
+        $actual = $this->parse('simple_template.html.twig');
 
         $this->assertEquals($expected, $actual);
     }
